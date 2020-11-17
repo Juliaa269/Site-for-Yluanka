c45f6db6d69afcb7d5fb0b71e43e9cf11d8413df
Julita <bilajulia998@gmail.com>
2020-11-18 00:17:51 +0200

    add burger menu

--- script.js
+++ script.js
@@ -6,12 +6,40 @@ $(document).ready(function(){
       return false;
     });
 });
-$(document).ready(function(){
-    $('a[href^="#"]').click(function(){ //берем все ссылки атрибу href которых начинается с #
-      var $element = $('a[name=' + $(this).attr('href').substr(1) + ']');
-      if($element.length == 1) { //на странице есть только 1 элемент с указанным якорем
-         $('html, body').animate({ scrollTop: $element.offset().top }, 2000); // анимируем скролл к элементу
-      }     
-      return false;
-    });
+// $(document).ready(function(){
+//     $('a[href^="#"]').click(function(){ //берем все ссылки атрибу href которых начинается с #
+//       let $element = $('a[name=' + $(this).attr('href').substr(1) + ']');
+//       if($element.length == 1) { //на странице есть только 1 элемент с указанным якорем
+//          $('html, body').animate({ scrollTop: $element.offset().top }, 2000); // анимируем скролл к элементу
+//       }     
+//       return false;
+//     });
+// });
+
+$(() => {
+  function burgerMenu(selector) {
+  let menu = $(selector);
+  let button = $('.burger-menu_button');
+  let links = $('.burger-menu_link');
+  let overlay = $('.burger-overlay');
+
+  button.on('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+
+  links.on('click', () => toggleMenu());
+  overlay.on('click', () => toggleMenu());
+
+  function toggleMenu() {
+    menu.toggleClass('burger-menu_active');
+    if(menu.hasClass('burger-menu_active')) {
+      $('body').css('overflow', 'hidden');
+    } else {
+      $('body').css('overflow', 'visible');
+    }
+  }
+}
+
+burgerMenu('.burger-menu');
 });
